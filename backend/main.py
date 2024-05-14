39a49683182bac91d3bf62c94b8994d2bb137bfd
Yair Riffkin <yair.riffkin@gmail.com>
2024-05-14 18:38:12 +0300

    nothing much. started using setLocalStorage hook

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 
-from flask import Flask
+from flask import Flask, g, jsonify
+import logging
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from database.db import close_db, init_db
 from views.auth import auth
 from views.users import users
+from views.items import items
 
 app = Flask(__name__)
 app.config.from_prefixed_env()
@@ -15,6 +17,23 @@ app.teardown_appcontext(close_db)
 
 app.register_blueprint(auth.bp)
 app.register_blueprint(users.bp)
+app.register_blueprint(items.bp)
+
+
+# @app.errorhandler(Exception)
+# def handle_exception(e):
+#     logging.exception("An error occurred:")
+#     return jsonify(error="An error occurred"), 500
+
+
+# @app.teardown_appcontext
+# def close_database_connection(exception=None):
+#     db = g.pop("db", None)
+#     if db is not None:
+#         db.close()
+#     if exception is not None:
+#         logging.error("Database error occurred:", exc_info=exception)
+
 
 print("starting the app")
 init_db()
